111025d23412fdada3c504a188b71df71f5385d9
MrWormy <thomas.laurenc@gmail.com>
2016-11-28 17:40:24 +0100

    Update main.c
    modification de la gestion des projectiles et amélioration de l'incrément de la vitesse + ajout d'un score caché

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-int ledPin = 12;
 int game[3][10][2] = {
   {
     {12, 9},
@@ -53,20 +52,66 @@ void light(int i, int j){
   digitalWrite(a, HIGH);
   pinMode(b, OUTPUT);
   digitalWrite(b, LOW);
-  delay(5);
+  delay(1);
   pinMode(a, INPUT);
   pinMode(b, INPUT);
 }
 
-int c = true;
+void lightProj(int i, int j){
+  if(j >= 0 && j < 11 && i < 3){
+    if(j < 10){
+      light(i,j);
+    } else {
+      delay(1);
+    }
+    if(j > 0) {
+      light(i,j - 1);
+    } else {
+      delay(1);
+    }
+  } else {
+    delay(2);
+  }
+}
+
+double nbCycles = 1.0;
+int cycles = 60;
+int cycle = 0;
+int score = 0;
+int speedUpPeriod = 6;
+int cyclesCount = 0;
+
+int projectiles[4][2] = {
+  {2, 10}, 
+  {0, 13},
+  {1, 16},
+  {0, 19}
+};
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if(c){
-    light(0,0);
-    c = false;
-  } else {
-    light(0,1);
-    c = true;
+  for(int k = 0; k < 4; k++){
+    if(projectiles[k][1] < 0){
+      projectiles[k][0] = (int) random(3);
+      projectiles[k][1] = 11;
+    }
+    lightProj(projectiles[k][0], projectiles[k][1]);
   }
+  
+  cycle++;
+  if(cycle > (cycles - nbCycles)) {
+    cycle = 0;
+    score++;
+    for(int k = 0; k < 4; k++){
+      projectiles[k][1]++;
+    }
+
+    cyclesCount++;
+    if(cyclesCount == speedUpPeriod){
+      nbCycles = nbCycles * 1.4;
+      cyclesCount = 0;
+    }
+  }
+
+  if(nbCycles > (cycles - 10)) {nbCycles = 1.0;}//fix vitesse < 0
 }
